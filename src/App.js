f9f0d5ba953b75fb0b6a3d2737fd7308ed0e26d4
swathirekha <swathirekha37@yahoo.in>
2021-10-25 09:17:55 -0500

    updated hatsPage and added navigation to hatsPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Switch,Route } from 'react-router';
+import { Link } from 'react-router-dom';
+
+const HatsPage = (props) => {
+  return (
+     <div>
+        <h1>
+            Hats Page
+        </h1>
+        
+      </div>
+  )
+}
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+         <Route exact path = "/" component = {HomePage}/>
+         <Route path = "/hats" component = {HatsPage}/>
+      </Switch>
     </div>
   );
 }
